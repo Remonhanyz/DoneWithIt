f9d4834a2ac1df741c526cce16094fde21843f1e
Remonhanyz <34352561+Remonhanyz@users.noreply.github.com>
2022-09-18 16:49:38 +0200

    18-9-2022

--- app/screens/ListingsScreen.js
+++ app/screens/ListingsScreen.js
@@ -1,25 +1,22 @@
 import {FlatList, StyleSheet, Text, View} from "react-native";
-import React from "react";
+import React, {useEffect, useState} from "react";
 import {SafeAreaView} from "react-native-safe-area-context";
 import Card from "../components/Card";
 import colors from "../config/colors";
 import routes from "../navigation/routes";
-const listings = [
-	{
-		id: 1,
-		title: "Red jacket for sale",
-		price: "100",
-		image: require("../assets/jacket.jpg")
-	},
-	{
-		id: 2,
-		title: "Couch in great condition",
-		price: "1000",
-		image: require("../assets/couch.jpg")
-	}
-];
+import listingsApi from "../api/listings";
 
 const ListingsScreen = ({navigation}) => {
+	const [listings, setListings] = useState([]);
+	const loadListings = async () => {
+		const response = listingsApi.getListings();
+		setListings(response.data);
+	};
+
+	useEffect(() => {
+		loadListings()
+	}, []);
+
 	return (
 		<SafeAreaView style={styles.container}>
 			<FlatList
@@ -30,7 +27,9 @@ const ListingsScreen = ({navigation}) => {
 						title={item.title}
 						subTitle={"$ " + item.price}
 						image={item.image}
-						onPress={() => navigation.navigate(routes.LISTING_DETAILS, item)}
+						onPress={() =>
+							navigation.navigate(routes.LISTING_DETAILS, item)
+						}
 					/>
 				)}
 			/>
