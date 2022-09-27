35227101ecca73659c1e301c84fd2c42dd896b91
Remonhanyz <34352561+Remonhanyz@users.noreply.github.com>
2022-09-27 16:57:35 +0200

    27-9-2022

--- app/screens/UploadScreen.js
+++ app/screens/UploadScreen.js
@@ -1,12 +1,29 @@
 import {Modal, StyleSheet, Text, View} from "react-native";
 import React from "react";
 import AppText from "../components/AppText";
+import * as Progress from "react-native-progress";
+import colors from "../config/colors";
+import AnimatedLottieView from "lottie-react-native";
 
-const UploadScreen = ({progress=0, visible=false}) => {
+const UploadScreen = ({progress = 0, visible = false, onDone}) => {
 	return (
 		<Modal visible={visible}>
 			<View style={styles.container}>
-				<AppText>{progress*100}</AppText>
+				{progress < 1 ? (
+					<Progress.Bar
+						progress={progress}
+						color={colors.primary}
+						width={200}
+					/>
+				) : (
+					<AnimatedLottieView
+						autoPlay
+						loop={false}
+							source={require("../assets/animations/done.json")}
+						style={styles.animation}
+						onAnimationFinish={onDone}
+					/>
+				)}
 			</View>
 		</Modal>
 	);
@@ -15,9 +32,12 @@ const UploadScreen = ({progress=0, visible=false}) => {
 export default UploadScreen;
 
 const styles = StyleSheet.create({
-    container: {
-        alignItems: "center",
-        // flex: '1',
-        justifyContent: 'center'
-    }
+	container: {
+		alignItems: "center",
+		flex: 1,
+		justifyContent: "center"
+	},
+	animation: {
+		width: 500
+	}
 });
